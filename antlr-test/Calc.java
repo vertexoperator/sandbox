dfb90c4139aab7db4a724d71959be746ea08630d
vertexoperator <miz@ubuntu.(none)>
2013-09-02 05:50:31 +0900

    add new evaluator

--- antlr-test/Calc.java
+++ antlr-test/Calc.java
@@ -7,11 +7,9 @@ import org.antlr.v4.runtime.BailErrorStrategy;
 import org.antlr.v4.runtime.misc.NotNull;
 import org.antlr.v4.runtime.tree.ErrorNode;
 import org.antlr.v4.runtime.tree.TerminalNode;
-import java.util.List;
 import java.lang.NumberFormatException;
 import java.math.BigInteger;
-import java.util.Stack;
-
+import java.util.*;
 
 public class Calc{
     public BigInteger eval(@NotNull String s){
@@ -23,196 +21,92 @@ public class Calc{
         parser.setErrorHandler( errHandler );
         parser.addParseListener(new ArithBaseListener());
         ArithParser.StartContext uu = parser.start();
-        return eval(uu.expr());
+        return new ExprNode(uu.expr()).eval();
     }
 
-    /* evalExprは再帰呼び出しを含んでいるので、非再帰バージョンも作っておく */
-    /*
-       data Node = Lit BigInteger
-                 | Op String
-                 | Expr ExprContext
-                 | MExpr MexprContext
-                 | Atom AtomContext
-    */
-    private static class Node {
-       private BigInteger _n = null;
-       private String _op = null; /* 四則演算 '+' , '-' , '*' , '/' */
-       private ArithParser.ExprContext _expr = null;
-       private ArithParser.MexprContext _mexpr = null;
-       private ArithParser.AtomContext _atom = null;
-
-       public Node(BigInteger n){
-          _n = n;
-       }
-       public Node(String op){
-          _op = op;
-       }
-       public Node(ArithParser.ExprContext expr){
-          _expr = expr;
-       }
-       public Node(ArithParser.MexprContext mexpr){
-          _mexpr = mexpr;
-       }
-       public Node(ArithParser.AtomContext atom){
-          _atom = atom;
-       }
-
-       public BigInteger INT(){
-          return _n;
-       }
-       public String op(){
-          return _op;
-       }
-       public ArithParser.ExprContext expr(){
-          return _expr;
-       }
-       public ArithParser.MexprContext mexpr(){
-          return _mexpr;
-       }
-       public ArithParser.AtomContext atom(){
-          return _atom;
-       }
-    }
-
-    private BigInteger eval(@NotNull ArithParser.ExprContext ctx){
-        BigInteger ret;
-        Stack<Node> stack = new Stack<Node>();
-        Stack<Node> args = new Stack<Node>();
-        stack.push(new Node(ctx));
-        while(true){
-            assert(!stack.empty());
-            Node it = stack.pop();
-            if(it.INT()!=null){
-                if(stack.empty()){
-                   ret = it.INT();
-                   break; /* 終了*/
-                }else{
-                   args.push(it);
-                }
-            }else if(it.expr()!=null){
-               ArithParser.ExprContext ec = it.expr();
-               if(ec.prefix!=null && ec.prefix.getText().equals("-")){
-                   args.push(new Node(new BigInteger("0")));
-                   args.push(new Node(ec.mexpr(0)));
-                   args.push(new Node("-"));
-               }else{
-                   args.push(new Node(ec.mexpr(0)));
-               }
-               List<ArithParser.AddsubContext> ops = ec.addsub();
-               for(int i=0 ; i<ops.size();i++){
-                   args.push(new Node(ec.mexpr(i+1)));
-                   if(ops.get(i).PLUS()!=null){
-                       args.push(new Node("+"));
-                   }else{
-                       args.push(new Node("-"));
-                   }
-               }
-               for(;!args.empty();){
-                   stack.push(args.pop());
-               }
-            }else if(it.mexpr()!=null){
-               ArithParser.MexprContext mc = it.mexpr();
-               List<ArithParser.MuldivContext> ops = mc.muldiv();
-               args.push( new Node(mc.atom(0)) );
-               for(int i=0 ; i<ops.size();i++){
-                   args.push( new Node(mc.atom(i+1)) );
-                   if(ops.get(i).MUL()!=null){
-                       args.push(new Node("*"));
-                   }else{
-                       args.push(new Node("/"));
-                   }
-               }
-               for(;!args.empty();){
-                   stack.push(args.pop());
-               }
-            }else if(it.atom()!=null){
-                ArithParser.AtomContext ac = it.atom();
-                if (ac.INT()!=null){
-                    String s = ac.INT().getSymbol().getText();
-                    stack.push(new Node(new BigInteger(s)));
-                }else{
-                    stack.push(new Node(ac.expr()));
-                }
-            }else if(it.op()!=null){
-                String op = it.op();
-                assert(!args.empty());
-                if(op.equals("+")){
-                    BigInteger rhs = args.pop().INT();
-                    BigInteger lhs = args.pop().INT();
-                    BigInteger res = lhs.add( rhs );
-                    stack.push( new Node(res) );
-                }else if(op.equals("-")){
-                    BigInteger rhs = args.pop().INT();
-                    BigInteger lhs = args.pop().INT();
-                    BigInteger res = lhs.subtract( rhs );
-                    stack.push( new Node(res) );
-                }else if(op.equals("*")){
-                    BigInteger rhs = args.pop().INT();
-                    BigInteger lhs = args.pop().INT();
-                    BigInteger res = lhs.multiply( rhs );
-                    stack.push( new Node(res) );
-                }else if(op.equals("/")){
-                    BigInteger rhs = args.pop().INT();
-                    BigInteger lhs = args.pop().INT();
-                    BigInteger res = lhs.divide( rhs );
-                    stack.push( new Node(res) );
-                }else{
-                  assert false: "illegal arithmetic operation? : " +op;
-                }
+    private static class ExprNode {
+       private List<String> ops;
+       private List<MexprNode> mexprs;
+       public ExprNode(ArithParser.ExprContext ctx){
+            ops = new ArrayList<String>();
+            mexprs = new ArrayList<MexprNode>();
+            if(ctx.prefix!=null && ctx.prefix.getText().equals("-")){
+                 ops.add("-");
             }else{
-                 assert(false);
+                 ops.add("+");
             }
-        }
-        return ret;
+            mexprs.add(new MexprNode(ctx.mexpr(0)));
+            List<ArithParser.AddsubContext> _ops = ctx.addsub();
+            for(int i=0 ; i<_ops.size();i++){
+                if(_ops.get(i).PLUS()!=null){
+                       ops.add("+");
+                }else{
+                       ops.add("-");
+                }
+                mexprs.add(new MexprNode(ctx.mexpr(i+1)));
+            } 
+       }
+       public BigInteger eval(){
+            BigInteger ret = new BigInteger("0");
+            for(int i = 0 ; i < ops.size() ; i++){
+               if(ops.get(i).equals("+")){
+                   ret = ret.add( mexprs.get(i).eval() );
+               }else{
+                   ret = ret.subtract( mexprs.get(i).eval() );
+               }
+            }
+            return ret;
+       }
     }
 
-    /* こっちは再帰版 */
-    private BigInteger evalExpr(@NotNull ArithParser.ExprContext ctx){
-           List<ArithParser.MexprContext> mexprs = ctx.mexpr();
-           List<ArithParser.AddsubContext> ops = ctx.addsub();
-           assert(mexprs.size() == ops.size()+1);
-           BigInteger ret = evalMexpr(mexprs.get(0));
-           if(ctx.prefix!=null && ctx.prefix.getText().equals("-")){
-               ret = ret.negate();
-           }
-           for(int i=0 ; i<ops.size();i++){
-              ArithParser.AddsubContext op = ops.get(i);
-              ArithParser.MexprContext mexpr = mexprs.get(i+1);
-              if(op.PLUS()!=null){
-                 ret = ret.add ( evalMexpr( mexpr ) );
-              }else{
-                 ret = ret.subtract( evalMexpr( mexpr ) );
-              }
-           }
-           return ret;
-    }
-    
-    private BigInteger evalMexpr(@NotNull ArithParser.MexprContext ctx){
-           List<ArithParser.AtomContext> atoms = ctx.atom();
-           List<ArithParser.MuldivContext> ops = ctx.muldiv();
-           assert(atoms.size() == ops.size()+1);
-           BigInteger ret = evalAtom(atoms.get(0));
-           for(int i=0 ; i<ops.size();i++){
-              ArithParser.MuldivContext op = ops.get(i);
-              ArithParser.AtomContext atom = atoms.get(i+1);
-              if(op.MUL()!=null){
-                 ret = ret.multiply( evalAtom( atom ) );
-              }else{
-                 ret = ret.divide( evalAtom( atom ) );
-              }
-           }
-           return ret;
+    private static class MexprNode{
+       private List<String> ops;
+       private List<AtomNode> atoms;
+
+       public MexprNode(ArithParser.MexprContext ctx){
+            ops = new ArrayList<String>();
+            atoms = new ArrayList<AtomNode>();
+            atoms.add( new AtomNode(ctx.atom(0)) );
+            List<ArithParser.MuldivContext> _ops = ctx.muldiv();
+            for(int i=0 ; i<_ops.size();i++){
+                if(_ops.get(i).MUL()!=null){
+                       ops.add("*");
+                }else{
+                       ops.add("/");
+                }
+                atoms.add(new AtomNode(ctx.atom(i+1)));
+            }
+       }
+
+       public BigInteger eval(){
+          BigInteger ret = atoms.get(0).eval();
+          for(int i = 0 ; i < ops.size() ; i++){
+               if(ops.get(i).equals("*")){
+                   ret = ret.multiply( atoms.get(i+1).eval() );
+               }else{
+                   ret = ret.divide( atoms.get(i+1).eval() );
+               }
+          }
+          return ret;
+       }
     }
 
-    private BigInteger evalAtom(@NotNull ArithParser.AtomContext ctx){
-        if (ctx.INT()!=null){
-           String s = ctx.INT().getSymbol().getText();
-           if(s.length() > 8){
-              throw new NumberFormatException("入力可能な数値は8桁までです:"+s);
-           }  
-           return new BigInteger(s);
-        }else{
-           return evalExpr(ctx.expr());
-        }
+    private static class AtomNode {
+       private BigInteger val = null;
+       private ExprNode expr = null;
+
+       public AtomNode(ArithParser.AtomContext ctx){
+            if (ctx.INT()!=null){
+                 String s = ctx.INT().getSymbol().getText();
+                 val = new BigInteger(s);
+            }else{
+                 expr = new ExprNode(ctx.expr());
+            }
+       }
+       public BigInteger eval(){
+            if(val!=null)return val;
+            return expr.eval();
+       }
     }
+
 }
